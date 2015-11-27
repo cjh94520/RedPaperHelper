137fe84971181ea86c8b30479fa14502e786474b
jiahui <892546937@qq.com>
2015-11-27 14:15:38 +0800

    帮助加入沉浸式状态栏

--- app/src/main/java/com/smartman/redpaperhelper/activity/HelpActivity.java
+++ app/src/main/java/com/smartman/redpaperhelper/activity/HelpActivity.java
@@ -1,9 +1,12 @@
 package com.smartman.redpaperhelper.activity;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.smartman.redpaperhelper.R;
+import com.smartman.redpaperhelper.utils.SystemBarUtil;
 
 /**
  * Created by Administrator on 2015/11/7.
@@ -14,5 +17,13 @@ public class HelpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_layout);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            SystemBarUtil.setTranslucentStatus(this, true);
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            // 使用颜色资源
+            tintManager.setStatusBarTintResource(R.color.red);
+        }
     }
 }
