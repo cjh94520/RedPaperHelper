006212d6260a5c2692363dd627388282aa8aa2c3
Jiahui <892546937@qq.com>
2015-12-28 21:44:04 +0800

    HelpActivity加入统计

--- app/src/main/java/com/smartman/redpaperhelper/activity/HelpActivity.java
+++ app/src/main/java/com/smartman/redpaperhelper/activity/HelpActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 
+import com.baidu.mobstat.StatService;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 import com.smartman.redpaperhelper.R;
 import com.smartman.redpaperhelper.utils.SystemBarUtil;
@@ -25,5 +26,19 @@ public class HelpActivity extends Activity {
             // 使用颜色资源
             tintManager.setStatusBarTintResource(R.color.red);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //打开百度统计
+        StatService.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //关闭百度统计
+        StatService.onPause(this);
     }
 }
